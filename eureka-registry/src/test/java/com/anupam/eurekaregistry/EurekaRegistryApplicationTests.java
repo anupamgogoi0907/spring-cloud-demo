c7c5bae0f2a30209ef68f552a5d7204bcfedd823
ac-agogoi <ac-agogoi@192.168.100.82>
2018-05-03 13:23:03 -0300

    pom changed

--- eureka-registry/src/test/java/com/anupam/eurekaregistry/EurekaRegistryApplicationTests.java
+++ eureka-registry/src/test/java/com/anupam/eurekaregistry/EurekaRegistryApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.anupam.registry.AppRegistry;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=AppRegistry.class)
 public class EurekaRegistryApplicationTests {
 
 	@Test
--- service-account/src/test/java/com/anupam/serviceaccount/ServiceAccountApplicationTests.java
+++ service-account/src/test/java/com/anupam/serviceaccount/ServiceAccountApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.anupam.account.AccountController;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=AccountController.class)
 public class ServiceAccountApplicationTests {
 
 	@Test
